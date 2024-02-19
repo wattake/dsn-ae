d778a7d2ee0701f46f55f4ee3268a30102c3611d
wattake <takew@asg.cs.tuat.ac.jp>
2024-02-19 18:45:15 +0000

    move exp3 correctly

--- exp3/exp3-run.sh
+++ exp3/exp3-run.sh
@@ -1,74 +1,65 @@
 #!/bin/bash
 
+if [ $# -ne 2 ];then
+    exit 1
+fi
 
-oses=(
-    "vp"
-    "das"
-    "fsm"
-    "netm"
-)
+os=$1
+app=$2
+
+# oses=(
+#     "uk"
+#     "vp"
+#     "das"
+#     "fsm"
+#     "netm"
+# )
 
 
-apps=(
-    "sqlite"
-    "nginx"
-    "redis"
-    "httpreply"
-)
+# apps=(
+#     "sqlite"
+#     "nginx"
+#     "nginx-separate"
+#     "redis"
+#     "redis-separate"
+#     "httpreply"
+#     "httpreply-separate"
+# )
 
 
-cd vp-app
+if [ ${os} = "uk" ]; then
+    cd uk-app
+else
+    cd vp-app
+fi
 
-make addbr
-
-for os in "${oses[@]}"; do
-    for app in "${apps[@]}"; do
-        # if [ ${app} = "nginx" ] || [ ${app} = "redis" ]; then
-        if [ ${app} = "sqlite" ]; then
-            cp ./sqlite-fs/test_org.db ./sqlite-fs/test.db
-            chmod 777 ./sqlite-fs/test.db
-            # there is no client-tool for sqlite
-        fi
-        if [ ${app} = "nginx" ]; then
-            # sleep 5 && ./../nginx_exp.sh http://172.44.0.2 &
-            make run OS=${os} APP=${app} BRG=br0 GADDR=192.168.200.1 ADDR=192.168.200.111 # remote
-        fi
-        if [ ${app} = "redis" ]; then
-            sleep 10 && ./../redis_exp.sh 172.44.0.1 &
-        fi
-        if [ ${app} = "httpreply" ]; then
-            # sleep 5 && ./../httpreply_exp.sh http://172.44.0.2:8123 &
-            make run-${app} OS=${os} APP=${app} BRG=br0 GADDR=192.168.200.1 ADDR=192.168.200.111 #remote
-        fi
-
-        make run-${app} OS=${os} APP=${app}
-    done
-done
-
-cd ../uk-app
-
-os=uk
-
-for app in "${apps[@]}"; do
-    # if [ ${app} = "nginx" ] || [ ${app} = "redis" ]; then
-    if [ ${app} = "sqlite" ]; then
-        # cp ./sqlite-fs/test_org.db ./sqlite-fs/test.db
-        # chmod 777 ./sqlite-fs/test.db
-        make run-${app} OS=${os} APP=${app}
-        # there is no client-tool for sqlite
-    fi
-    if [ ${app} = "nginx" ]; then
-        # sleep 5 && ./../nginx_exp.sh http://172.44.0.2 &
-        make run OS=${os} APP=${app} BRG=br0 GADDR=192.168.200.1 ADDR=192.168.200.111 # remote
-    fi
-    if [ ${app} = "redis" ]; then
-        sleep 10 && ./../redis_exp.sh 172.44.0.1 &
-    fi
-    if [ ${app} = "httpreply" ]; then
-        # sleep 5 && ./../httpreply_exp.sh http://172.44.0.2:8123 &
-        make run-${app} OS=${os} APP=${app} BRG=br0 GADDR=192.168.200.1 ADDR=192.168.200.111 #remote
-    fi
-    # make run-${app} OS=${os} APP=${app}
-done
-
-make delbr
+if [ ${app} = "sqlite" ]; then
+    make run-${app} OS=${os} APP=${app}
+fi
+if [ ${app} = "nginx" ]; then
+    make addbr
+    sleep 10 && ./../nginx_exp.sh http://172.44.0.2 &
+    make run-nginx OS=${os} APP=nginx # remote
+    make delbr
+fi
+if [ ${app} = "nginx-separate" ]; then
+    make run-nginx OS=${os} APP=${app} BRG=br0 GADDR=192.168.200.1 ADDR=192.168.200.111 # remote
+fi
+if [ ${app} = "redis" ]; then
+    make addbr
+    sleep 10 && ./../redis_exp.sh 172.44.0.2 &
+    make run-redis OS=${os} APP=redis
+    make delbr
+fi
+if [ ${app} = "redis-separate" ]; then
+    make run-redis OS=${os} APP=redis BRG=br0 GADDR=192.168.200.1 ADDR=192.168.200.111 #remote
+fi
+if [ ${app} = "httpreply" ]; then
+    make addbr
+    sleep 5 && ./../httpreply_exp.sh http://172.44.0.2:8123 &
+    make run-httpreply OS=${os} APP=httpreply
+    make delbr
+fi
+if [ ${app} = "httpreply-separate" ]; then
+    make run-httpreply OS=${os} APP=httpreply BRG=br0 GADDR=192.168.200.1 ADDR=192.168.200.111 #remote
+fi
--- exp3/redis_exp.sh
+++ exp3/redis_exp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-clents=2
-pipeline=2
-requests=2
+clents=4
+pipeline=4
+requests=1000000
 # address=http://192.168.200.111/
 
 if [ $# -ne 1 ];then
     exit 1
 fi
 
-redis-benchmark -h $1 -c $clents -P $pipeline -r 100000 -t get -n $requests --csv
+redis-benchmark -h $1 -c $clents -P $pipeline -r 100000 -t set -n $requests --csv
 # redis-cli -h $1 PING > /dev/null
