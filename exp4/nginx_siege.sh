85619342b22d17f92f5f635d8ce5d6306cce15e4
wattake <takew@asg.cs.tuat.ac.jp>
2024-02-23 18:02:56 +0000

    small fix in exp4

--- exp4/nginx_siege.sh
+++ exp4/nginx_siege.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/bash
 
-connection=30
-thread=16
-duration=1m
 # address = "http://192.168.200.111/"
 
 if [ $# -ne 1 ];then
     exit 1
 fi
 
-for ((i=0; i<10; i++))
+for ((i=0; i<20; i++))
 do
-  siege -c 255 -t 1S $1 #| grep availability
+  siege -c 100 -t 1s $1 | grep availability
 done
